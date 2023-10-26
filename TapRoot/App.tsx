2d109037c9c9953d1747c95406298d9b222b892a
Edward Bender <59852058+bendered2332@users.noreply.github.com>
2023-10-26 17:21:55 -0400

    Testing out database changes

--- TapRoot/App.tsx
+++ TapRoot/App.tsx
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import React from 'react';
+
+const doc = firestore().collection("Stuff").doc("Hello");
 
 export default function App() {
+  const [text, setText] = React.useState("loading...");
+
+  React.useEffect(() => {
+    doc.get().then(docSnapshot => {
+      if(docSnapshot.exists){
+        setText(JSON.stringify(docSnapshot.data(), null, 2));
+      }else{
+        setText(
+          "the document Stuff/Hello doesn't exists :/\n" +
+          "Create it in Firestore and try again"
+        );
+      }
+    }).catch((e) => {
+      console.warn(e);
+      setText("Failed :(");
+    });
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Your firebase is configured!</Text>
+      <Text> FireStore Stuff/Hello:</Text>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
