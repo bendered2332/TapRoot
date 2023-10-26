c0a78931e151fb397fb7cbd1aee5d65502a214a8
Edward Bender <59852058+bendered2332@users.noreply.github.com>
2023-10-26 17:41:56 -0400

    Added metro.config.ts and installed firebase

---
+++ TapRoot/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDCv9y-buDbsVEx9Hy4ePuFbsAnIw5-tQ",
+  authDomain: "taproot-561bd.firebaseapp.com",
+  projectId: "taproot-561bd",
+  storageBucket: "taproot-561bd.appspot.com",
+  messagingSenderId: "708016590340",
+  appId: "1:708016590340:web:b4525c7a0210b6898eaf51"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
