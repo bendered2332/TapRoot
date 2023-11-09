b1e08507d858fb67b9f129d180e0565ae1499a22
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2023-11-09 12:19:52 -0500

    template table

--- TapRoot/src/Components/History.tsx
+++ TapRoot/src/Components/History.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import mockData from '../mockData/mockData.json';
 import { View, Text, Button } from 'react-native';
+import HistoryTable from '../SubComponents/historyTable';
 
 function getData() {
   console.log(mockData.data[2]);
@@ -11,6 +12,7 @@ const History = () => {
   return (
     <View>
       <Text>This is the History screen which will show past api data in chart form??????TBD</Text>
+      <HistoryTable></HistoryTable>
     </View>
   );
 };
