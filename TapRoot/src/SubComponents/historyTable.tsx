f9535302ad8cf1e601ee1d654dd777229df5f04e
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2023-11-14 14:03:41 -0500

    reformat json - sorting of table by date

--- TapRoot/src/SubComponents/historyTable.tsx
+++ TapRoot/src/SubComponents/historyTable.tsx
@@ -3,16 +3,22 @@ import { DataTable } from 'react-native-paper';
 import mockData from '../mockData/mockData.json';
 
 const HistoryTable = () => {
+  interface exData {
+    date: string;
+    time: string;
+    humidity: number;
+    id: number;
+  }
+  console.log(mockData.data[4]);
   const [page, setPage] = React.useState<number>(0);
   const [numberOfItemsPerPageList] = React.useState([5, 10]);
   const [itemsPerPage, onItemsPerPageChange] = React.useState(
     numberOfItemsPerPageList[0]
   );
-
-  const [items] = React.useState(
+  const [sortDirection, setSortDirection] = React.useState<'ascending' | 'descending'>('descending');
+  const [items, setItems] = React.useState(
     mockData.data
   );
-
   const from = page * itemsPerPage;
   const to = Math.min((page + 1) * itemsPerPage, items.length);
 
@@ -20,16 +26,46 @@ const HistoryTable = () => {
     setPage(0);
   }, [itemsPerPage]);
 
+  const sortDateData = (direction: 'ascending' | 'descending') => {
+    const sortedItems = [...items].sort((a: exData, b: exData) => {
+      if (direction === 'ascending') {
+        return a.date.localeCompare(b.date); 
+      } else {
+         return b.date.localeCompare(a.date);
+      }
+    });
+    setItems(sortedItems);
+  };
+  const handleSort = (key: string) => {
+    const newSortDirection = sortDirection === 'ascending' ? 'descending' : 'ascending';
+    console.log("new: ", newSortDirection, "old: ",sortDirection);
+    setSortDirection(newSortDirection);
+    switch(key) { 
+      case 'date': { 
+        sortDateData(newSortDirection);
+         break; 
+      } 
+      case 'humidity': { 
+         // to add humidity sorter or by time sorter
+         break; 
+      } 
+   } 
+  };
+
   return (
     <DataTable>
-      <DataTable.Header>
-        <DataTable.Title>Date</DataTable.Title>
+      <DataTable.Header>     
+      <DataTable.Title 
+         onPress={() => handleSort('date')} 
+         sortDirection={sortDirection === 'ascending' ? 'ascending' : 'descending'}>Date</DataTable.Title>
+        <DataTable.Title>Time</DataTable.Title>
         <DataTable.Title numeric>Humidity</DataTable.Title>
       </DataTable.Header>
 
       {items.slice(from, to).map((item, i) => ( 
         <DataTable.Row key={i}>
           <DataTable.Cell>{item.date}</DataTable.Cell>
+          <DataTable.Cell>{item.time?.substring(0,5)}</DataTable.Cell> 
           <DataTable.Cell numeric>{item.humidity.toFixed(2)}</DataTable.Cell>
         </DataTable.Row>
       ))}
