712f5a3355d025560e87ec30063df5e03417a162
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2024-01-25 16:41:16 -0500

    chart component added

--- TapRoot/src/Components/EditThreshold.tsx
+++ TapRoot/src/Components/EditThreshold.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet} from 'react-native';
+import ThresholdChart from '../SubComponents/thresholdChart';
 
 const EditThreshold = () => {
   return (
     <View style={styles.container}>
       <Text>This is the Edit Threshold screen where you can se the threshold and get notifications on when it hits! </Text>
+      <ThresholdChart></ThresholdChart>
     </View>
   );
 };
---
+++ TapRoot/src/SubComponents/thresholdChart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet} from 'react-native';
+ 
+const ThresholdChart = () => {
+    return (
+      <View>
+        <Text>THIS TEXT IS FROM THE CHART COMPONENT </Text>
+        <Text>CHART WILL BE HERE</Text>
+      </View>  
+    );
+};
+export default ThresholdChart;
