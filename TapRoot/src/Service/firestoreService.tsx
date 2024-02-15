0e875e3a928448f2b38ae73b0bb9cb521665d844
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2024-02-15 17:19:57 -0500

    function added to get latest data

--- TapRoot/src/Service/firestoreService.tsx
+++ TapRoot/src/Service/firestoreService.tsx
@@ -5,7 +5,7 @@ import { FIRESTORE_DB } from '../../firebaseConfig';
 
 class FirestoreService {
   // fetch data from Firestore collection and document
-  async getData(collectionName: string, documentId: string): Promise<DataEntry[] | null> {
+  async getAllData(collectionName: string, documentId: string): Promise<DataEntry[] | null> {
     try {
       const docRef = doc(FIRESTORE_DB, collectionName, documentId);
       const docSnapshot = await getDoc(docRef);
@@ -13,7 +13,8 @@ class FirestoreService {
       if (docSnapshot.exists()) {
         const data = docSnapshot.data() as DataEntry[];
         console.log("Document found");
-        return data.data; // .data is returning the variable that contians the array
+        let result = data.data; // .data is returning the variable that contians the array
+        return result; 
       } else {
         console.log("Document not found");
         return null;
@@ -24,6 +25,27 @@ class FirestoreService {
     }
   }
   
+  // get the latest data from db - last element of 'data' array
+  async getLatestData(collectionName: string, documentId: string): Promise<DataEntry | null> {
+    try {
+      const docRef = doc(FIRESTORE_DB, collectionName, documentId);
+      const docSnapshot = await getDoc(docRef);
+  
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data() as DataEntry[];
+        console.log("Document found");
+        let result = data.data; // .data is returning the variable that contians the array
+        let lastElement = result[result.length - 1];
+        return lastElement; 
+      } else {
+        console.log("Document not found");
+        return null;
+      }
+    } catch (error) {
+      console.error("Error fetching document:", error);
+      throw error;
+    }
+  }
 }
 
 export default FirestoreService;
--- TapRoot/src/SubComponents/TestComponent.tsx
+++ TapRoot/src/SubComponents/TestComponent.tsx
@@ -5,15 +5,16 @@ import DataService from '../Service/firestoreService';
 
 const YourComponent: React.FC = () => {
   const [dataEntries, setDataEntries] = useState<DataEntry[] | null>(null); // set null
+  const [lastEntry, setLastEntry] = useState<DataEntry | null>(null);
   const documentName = "DataEntry";
   const collectionName = "Data";
+  const dataService = new DataService();
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchAllData = async () => {
       try {
-        const dataService = new DataService();
         //console.log("service created making data request")
-        const data = await dataService.getData(collectionName, documentName);
+        const data = await dataService.getAllData(collectionName, documentName);
         console.log("Data is back to the component page");
 
         if (data) {
@@ -25,13 +26,31 @@ const YourComponent: React.FC = () => {
       }
     };
 
-    fetchData();
+    fetchAllData();
   }, []);
 
+  useEffect(() => {
+    const fetchLatestData = async () => {
+      try {
+        const data = await dataService.getLatestData(collectionName, documentName);
+
+        if (data) {
+          setLastEntry(data)
+          console.log("latest Data: ",data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchLatestData();
+  }, []);
+
+
   dataEntries? console.log("Is not NUll"): console.log("IS NULL");
-  dataEntries?.map((entry: DataEntry, index: number) => {
-    console.log(entry);
-  });
+  // dataEntries?.map((entry: DataEntry, index: number) => {
+  //   console.log(entry);
+  // });
 
   return (
     <View>
